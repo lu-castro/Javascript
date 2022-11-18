c4eac8d8ce1e8e32095d99d33943c56d7f348d9a
lu-castro <luciacastro.23.5@gmail.com>
2022-11-18 16:31:09 -0300

    update

---
+++ controlador.js
@@ -0,0 +1,22 @@
+var edad = document.getElementById("edad").value;
+
+function calcularEdad(){
+    var edad = document.getElementById("edad").value;
+    if(edad >= 18){
+        alert("Podés entrar a esta página");
+    } else {
+        alert("Vos no podés entrar a esta página");
+    }
+    
+}
+
+function edadAlice() {
+    var edadAlice = document.getElementById("edadAlice").value;
+    if (edadAlice < 18) {
+        alert("Alice es menor de edad");
+    } else if (edadAlice >= 18 && edadAlice <= 21) {
+        alert("Alice tiene entre 18 y 21");
+    } else {
+        alert("Alice tiene más de 21");
+    }
+}
